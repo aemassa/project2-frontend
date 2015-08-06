8a9047e179786f525d297bf7ed76fe11d970a174
aemassa <annaemassa@gmail.com>
2015-08-06 10:38:23 -0400

    Adds basic CRUD functionality for events.
    Uses dummy front-end.

--- main.js
+++ main.js
@@ -2,14 +2,13 @@
 $(function() {
     "use strict";
     var sa = '//localhost:3000';
-    var myToken;
+    //var myToken;
 
     $("#sign-up-button").on("click", function(e) {
         $.ajax(sa + "/users", {
             contentType: "application/json",
             data: JSON.stringify({
                 credentials: {
-                    //name: $("#exampleInputName1").val(),
                     email: $("#exampleInputEmail1").val(),
                     password: $("#exampleInputPassword1").val(),
                     password_confirmation: $("#exampleInputPassword1").val()
@@ -23,11 +22,9 @@ $(function() {
             processData: false
         })
         .done(function(data, textStatus, jqXHR) {
-            //$("#result").val(JSON.stringify(data));
             console.log(JSON.stringify(data));
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
-            //$("#result").val("registration failed");
             console.log('Registration failed.');
         })
         .always();
@@ -48,13 +45,11 @@ $(function() {
             processData: false
         })
         .done(function(data, textStatus, jqXHR) {
-            //$("#token").val(data.token);
-            myToken = data.token;
-            //simpleStorage.set('token', data.token);
+            //myToken = data.token;
+            simpleStorage.set('token', data.token);
             console.log(data.token);
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
-            //$("#result").val("login failed");
             console.log('Login failed.');
         })
         .always();
@@ -65,123 +60,228 @@ $(function() {
             dataType: "json",
             method: "GET",
             headers: {
-              Authorization: 'Token token=' + myToken
+              Authorization: 'Token token=' + simpleStorage.get('token')
             }
         })
         .done(function(data, textStatus, jqXHR) {
-            //$("#result").val(JSON.stringify(data));
             console.log(JSON.stringify(data));
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
-            //$("#result").val("login failed");
             console.log('Failed to list events.')
         })
         .always();
     });
 
-    $("#create").on("click", function(e) {
-        $.ajax(sa + "/events", {
-            contentType: "application/json",
-            processData: false,
-            data: JSON.stringify({}),
-            dataType: "json",
-            method: "POST",
-            headers: {
-              Authorization: 'Token token=' + $('#token').val()
-            }
-        })
-        .done(function(data, textStatus, jqXHR) {
-            $("#result").val(JSON.stringify(data));
-        })
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            $("#result").val("create failed");
-        })
-        .always();
-    });
+    // $("#create-event-button").on("click", function(e) {
+    //     $.ajax(sa + "/events", {
+    //         contentType: "application/json",
+    //         processData: false,
+    //         data: JSON.stringify({
+    //           event: {
+    //               description: $("#inputEventDescription").val(),
+    //               date: $("#inputEventDate").val(),
+    //               time: $("#inputEventTime").val(),
+    //               location: $("#inputEventLocation").val(),
+    //           }
+    //         }),
+    //         dataType: "json",
+    //         method: "POST",
+    //         headers: {
+    //           Authorization: 'Token token=' + simpleStorage.get('token')
+    //         }
+    //     })
+    //     .done(function(data, textStatus, jqXHR) {
+    //         //$("#result").val(JSON.stringify(data));
+    //         console.log(JSON.stringify(data));
+    //     })
+    //     .fail(function(jqXHR, textStatus, errorThrown) {
+    //         //$("#result").val("create failed");
+    //         console.log('Failed to create event.')
+    //     })
+    //     .always();
+    // });
 
-    $("#show").on("click", function(e) {
-        $.ajax(sa + "/games/" + $('#id').val(), {
-            dataType: "json",
-            method: "GET",
-            headers: {
-              Authorization: 'Token token=' + $('#token').val()
-            }
-        })
-        .done(function(data, textStatus, jqXHR) {
-            $("#result").val(JSON.stringify(data));
-        })
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            $("#result").val("show failed");
-        })
-        .always();
-    });
-
-    $("#join").on("click", function(e) {
-      $.ajax(sa + "/games/" + $('#id').val(), {
+    $("#event-create").on("click", function(e) {
+      $.ajax(sa + "/events", {
           contentType: "application/json",
           processData: false,
-          data: JSON.stringify({}),
-          dataType: "json",
-          method: "PATCH",
-          headers: {
-            Authorization: 'Token token=' + $('#token').val()
+        data: JSON.stringify({
+          event: {
+              description: $("#event-description").val(),
+              date: $("#event-date").val(),
+              time: $("#event-time").val(),
+              location: $("#event-location").val(),
+          }
+        }),
+        dataType: "json",
+        method: "POST",
+        headers: {
+          Authorization: 'Token token=' + simpleStorage.get('token')
         }
     })
-      .done(function(data, textStatus, jqXHR) {
-          $("#result").val(JSON.stringify(data));
-      })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-          $("#result").val("join failed");
-      })
-      .always();
+    .done(function(data, textStatus, jqXHR) {
+      //$("#result").val(JSON.stringify(data));
+      console.log(JSON.stringify(data));
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      //$("#result").val("create failed");
+      console.log('Failed to create event.')
+    })
+    .always();
   });
 
-$("#move").on("click", function(e) {
-      $.ajax(sa + "/games/" + $('#id').val(), {
+    $("#event-update").on("click", function(e) {
+      $.ajax(sa + "/events/" + $("#event-id").val(), {
           contentType: "application/json",
           processData: false,
-          data: JSON.stringify({
-            game: {
-                cell: {
-                index: +$('#index').val(),
-                value: $('#value').val()
-                }
-            }
-          }),
-          dataType: "json",
-          method: "PATCH",
-          headers: {
-            Authorization: 'Token token=' + $('#token').val()
+        data: JSON.stringify({
+          event: {
+              description: $("#event-description").val(),
+              date: $("#event-date").val(),
+              time: $("#event-time").val(),
+              location: $("#event-location").val()
+          }
+        }),
+        dataType: "json",
+        method: "PATCH",
+        headers: {
+          Authorization: 'Token token=' + simpleStorage.get('token')
         }
     })
-      .done(function(data, textStatus, jqXHR) {
-          $("#result").val(JSON.stringify(data));
-      })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-          $("#result").val("move failed");
-      })
-      .always();
+    .done(function(data, textStatus, jqXHR) {
+      //$("#result").val(JSON.stringify(data));
+      console.log('Event updated!')
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      //$("#result").val("create failed");
+      console.log('Failed to update event.')
+    })
+    .always();
   });
 
-    $('#watch').on('click', function() {
-        gameWatcher = resourceWatcher(sa + "/games/" + $('#id').val() + '/watch', {
-            Authorization: 'Token token=' + $('#token').val()
-        });
-        gameWatcher.on('change', function(data){
-            var parsedData = JSON.parse(data);
-            // heroku routers report this as a 503
-            // if (data.timeout) { //not an error
-            // gameWatcher.close();
-            // return console.warn(data.timeout);
-            // }
-            var gameData = parsedData.game;
-            var cell = gameData.cell
-            $('#index').val(cell.index);
-            $('#value').val(cell.value);
-        });
-        gameWatcher.on('error', function(e){
-            console.error('an error has occured with the stream', e);
-        });
-    });
+    // $("#event-update").on('click', function(){
+    //   $.ajax({
+    //     url: '/events/' + $("#event-id").val(),
+    //     method: 'PATCH',
+    //     data: {
+    //       event: {
+    //         description: $("#event-description").val(),
+    //         date: $("#event-date").val(),
+    //         time: $("#event-time").val(),
+    //         location: $("#event-location").val()
+    //       }
+    //     }
+    //     }).done(function(data){
+    //       console.log("Updated event!");
+    //     }).fail(function(data){
+    //       console.error(data);
+    //     });
+    //   });
+
+    $("#event-destroy").on("click", function(e) {
+      $.ajax(sa + "/events/" + $("#event-id").val(), {
+          contentType: "application/json",
+          processData: false,
+          method: "DELETE",
+          headers: {
+            Authorization: 'Token token=' + simpleStorage.get('token')
+        }
+    })
+    .done(function(data, textStatus, jqXHR) {
+      //$("#result").val(JSON.stringify(data));
+      console.log('Event destroyed!')
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      //$("#result").val("create failed");
+      console.log('Failed to destroy event.')
+    })
+    .always();
+  });
+
+
+//     $("#show").on("click", function(e) {
+//         $.ajax(sa + "/games/" + $('#id').val(), {
+//             dataType: "json",
+//             method: "GET",
+//             headers: {
+//               Authorization: 'Token token=' + $('#token').val()
+//             }
+//         })
+//         .done(function(data, textStatus, jqXHR) {
+//             $("#result").val(JSON.stringify(data));
+//         })
+//         .fail(function(jqXHR, textStatus, errorThrown) {
+//             $("#result").val("show failed");
+//         })
+//         .always();
+//     });
+
+//     $("#join").on("click", function(e) {
+//       $.ajax(sa + "/games/" + $('#id').val(), {
+//           contentType: "application/json",
+//           processData: false,
+//           data: JSON.stringify({}),
+//           dataType: "json",
+//           method: "PATCH",
+//           headers: {
+//             Authorization: 'Token token=' + $('#token').val()
+//         }
+//     })
+//       .done(function(data, textStatus, jqXHR) {
+//           $("#result").val(JSON.stringify(data));
+//       })
+//       .fail(function(jqXHR, textStatus, errorThrown) {
+//           $("#result").val("join failed");
+//       })
+//       .always();
+//   });
+
+// $("#move").on("click", function(e) {
+//       $.ajax(sa + "/games/" + $('#id').val(), {
+//           contentType: "application/json",
+//           processData: false,
+//           data: JSON.stringify({
+//             game: {
+//                 cell: {
+//                 index: +$('#index').val(),
+//                 value: $('#value').val()
+//                 }
+//             }
+//           }),
+//           dataType: "json",
+//           method: "PATCH",
+//           headers: {
+//             Authorization: 'Token token=' + $('#token').val()
+//         }
+//     })
+//       .done(function(data, textStatus, jqXHR) {
+//           $("#result").val(JSON.stringify(data));
+//       })
+//       .fail(function(jqXHR, textStatus, errorThrown) {
+//           $("#result").val("move failed");
+//       })
+//       .always();
+//   });
+
+//     $('#watch').on('click', function() {
+//         gameWatcher = resourceWatcher(sa + "/games/" + $('#id').val() + '/watch', {
+//             Authorization: 'Token token=' + $('#token').val()
+//         });
+//         gameWatcher.on('change', function(data){
+//             var parsedData = JSON.parse(data);
+//             // heroku routers report this as a 503
+//             // if (data.timeout) { //not an error
+//             // gameWatcher.close();
+//             // return console.warn(data.timeout);
+//             // }
+//             var gameData = parsedData.game;
+//             var cell = gameData.cell
+//             $('#index').val(cell.index);
+//             $('#value').val(cell.value);
+//         });
+//         gameWatcher.on('error', function(e){
+//             console.error('an error has occured with the stream', e);
+//         });
+//     });
 
 });
